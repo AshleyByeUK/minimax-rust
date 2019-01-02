6a3cb35f497367220c7f7d7371de3f8d4b19b672
Ashley Bye <ashley+git@ashleybye.uk>
2019-01-02 13:08:58 +0000

    compute scorce for one child at n depth

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,7 @@
 // Build a k-ary tree.
 // 1. TDD out creating a node and adding children to a node.
+// 2. Link nodes and determine score of top-most node. Linked list. Root node has score of 5. PAss into score function, has score of 5.
+// Root has score of 5
 #[derive(Debug, PartialEq)]
 struct TreeNode {
     children: Box<Option<Vec<TreeNode>>>,
@@ -10,6 +12,16 @@ impl TreeNode {
     fn new(children: Option<Vec<TreeNode>>, score: Option<i32>) -> TreeNode {
         TreeNode { children: Box::new(children), score }
     }
+
+    fn score(&self) -> Option<i32> {
+        match self.score {
+            None => match self.children.as_ref() {
+                None => None,
+                Some(children) => children[0].score()
+            },
+            Some(score) => Some(score)
+        }
+    }
 }
 
 #[cfg(test)]
@@ -17,19 +29,11 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_node_with_zero_child_nodes_and_no_score() {
+    fn create_node_with_zero_child_nodes() {
         let node = TreeNode::new(None, None);
 
         assert_eq!(*node.children, None);
-        assert_eq!(node.score, None);
-    }
-
-    #[test]
-    fn create_node_with_zero_child_nodes_and_a_score() {
-        let node = TreeNode::new(None, Some(1));
-
-        assert_eq!(*node.children, None);
-        assert_eq!(node.score, Some(1));
+        assert_eq!(node.score(), None);
     }
 
     #[test]
@@ -38,7 +42,7 @@ mod tests {
         let node = TreeNode::new(Some(vec![child]), None);
 
         assert_eq!(*node.children, Some(vec![TreeNode::new(None, None)]));
-        assert_eq!(node.score, None);
+        assert_eq!(node.score(), None);
     }
 
     #[test]
@@ -48,6 +52,30 @@ mod tests {
         let node = TreeNode::new(Some(vec![child1, child2]), None);
 
         assert_eq!(*node.children, Some(vec![TreeNode::new(None, None), TreeNode::new(None, None)]));
-        assert_eq!(node.score, None);
+        assert_eq!(node.score(), None);
+    }
+
+    #[test]
+    fn scores_node_with_no_children() {
+        let parent = TreeNode::new(None, Some(10));
+
+        assert_eq!(parent.score(), Some(10));
+    }
+
+    #[test]
+    fn scores_node_with_one_child() {
+        let child = TreeNode::new(None, Some(10));
+        let parent = TreeNode::new(Some(vec![child]), None);
+
+        assert_eq!(parent.score(), Some(10));
+    }
+
+    #[test]
+    fn scores_node_with_multiple_children() {
+        let grandchild = TreeNode::new(None, Some(10));
+        let child = TreeNode::new(Some(vec![grandchild]), None);
+        let parent = TreeNode::new(Some(vec![child]), None);
+
+        assert_eq!(parent.score(), Some(10));
     }
 }
