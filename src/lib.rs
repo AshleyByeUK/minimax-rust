58e59c16ea4f360c8f9f63616d1081e8addc489d
Ashley Bye <ashley+git@ashleybye.uk>
2019-01-02 10:09:59 +0000

    add score to tree node

--- src/lib.rs
+++ src/lib.rs
@@ -3,11 +3,12 @@
 #[derive(Debug, PartialEq)]
 struct TreeNode {
     children: Box<Option<Vec<TreeNode>>>,
+    score: Option<i32>,
 }
 
 impl TreeNode {
-    fn new(children: Option<Vec<TreeNode>>) -> TreeNode {
-        TreeNode { children: Box::new(children) }
+    fn new(children: Option<Vec<TreeNode>>, score: Option<i32>) -> TreeNode {
+        TreeNode { children: Box::new(children), score }
     }
 }
 
@@ -16,37 +17,37 @@ mod tests {
     use super::*;
 
     #[test]
-    fn create_node_with_zero_child_nodes() {
-        let node = TreeNode::new(None);
+    fn create_node_with_zero_child_nodes_and_no_score() {
+        let node = TreeNode::new(None, None);
 
-        assert_eq!(node, TreeNode { children: Box::new(None) });
+        assert_eq!(*node.children, None);
+        assert_eq!(node.score, None);
+    }
+
+    #[test]
+    fn create_node_with_zero_child_nodes_and_a_score() {
+        let node = TreeNode::new(None, Some(1));
+
+        assert_eq!(*node.children, None);
+        assert_eq!(node.score, Some(1));
     }
 
     #[test]
     fn create_node_with_one_child_node() {
-        let child = TreeNode::new(None);
+        let child = TreeNode::new(None, None);
+        let node = TreeNode::new(Some(vec![child]), None);
 
-        let node = TreeNode::new(Some(vec![child]));
-
-        assert_eq!(node, TreeNode {
-            children: Box::new(Some(vec![
-                TreeNode { children: Box::new(None) }
-            ]))
-        });
+        assert_eq!(*node.children, Some(vec![TreeNode::new(None, None)]));
+        assert_eq!(node.score, None);
     }
 
     #[test]
     fn create_node_with_multiple_child_nodes() {
-        let child1 = TreeNode::new(None);
-        let child2 = TreeNode::new(None);
+        let child1 = TreeNode::new(None, None);
+        let child2 = TreeNode::new(None, None);
+        let node = TreeNode::new(Some(vec![child1, child2]), None);
 
-        let node = TreeNode::new(Some(vec![child1, child2]));
-
-        assert_eq!(node, TreeNode {
-            children: Box::new(Some(vec![
-                TreeNode { children: Box::new(None) },
-                TreeNode { children: Box::new(None) },
-            ]))
-        });
+        assert_eq!(*node.children, Some(vec![TreeNode::new(None, None), TreeNode::new(None, None)]));
+        assert_eq!(node.score, None);
     }
 }
